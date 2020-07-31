9dbb83643906410a7f1d563dcfe1cc97f7466059
EricPoehlsen <eric@eric-poehlsen>
2020-07-31 12:10:53 +0200

    Search and toggle reimplemented in TraitSelectFragment
    * Traits can be searched by name
    * Toggle between all traits, advantages and disadvantages

--- app/src/main/java/de/aequinoktium/twedit/TraitSelectFragment.kt
+++ app/src/main/java/de/aequinoktium/twedit/TraitSelectFragment.kt
@@ -36,13 +36,13 @@ class TraitSelectFragment : Fragment() {
     private lateinit var rv_container: RecyclerView
     private lateinit var rv_manager: LinearLayoutManager
     private lateinit var rv_adapter: TraitSelectAdapter
-
-
-
     private lateinit var te_search: EditText
     private lateinit var b_toggle: Button
     private var traits = arrayOf<TraitData>()
 
+    /**
+     * Creates the view for this Fragment
+     */
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -51,6 +51,9 @@ class TraitSelectFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_trait_select, container, false)
     }
 
+    /**
+     * Initiates the View once it has been created
+     */
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -81,63 +84,41 @@ class TraitSelectFragment : Fragment() {
         te_search.addTextChangedListener(watcher)
     }
 
+    /**
+     * filters the trait list based on search and (dis)advantages toggle button
+     */
     fun toggleTraits(view: View){
-        /*
+        var filtered_traits = arrayOf<TraitData>()
+
         if (view == b_toggle) {
             toggle += 1
             if (toggle > 2) toggle = 0
         }
-        var search = te_search.text.toString().toLowerCase()
+        var search = te_search.text.toString().toLowerCase(c.LOCALE)
 
-        when (toggle) {
-            0 -> {
+        for (trait in traits) {
+            val name = trait.name.toLowerCase(c.LOCALE)
+            if (search.length > 0 && search !in name) continue
+
+            if (toggle == 0) {
+                filtered_traits += trait
                 b_toggle.text = getText(R.string.ts_all)
-                for (v in ll_container.children) {
-                    val trait = v as TraitView
-                    trait.visibility = View.VISIBLE
-
-                    val name = trait.getName().toLowerCase()
-                    if (search.length >= 1 && search !in name) {
-                        trait.visibility = View.GONE
-                    }
-                }
-            }
-            1 -> {
+            } else if (toggle == 1 && trait.xp > 0) {
+                filtered_traits += trait
                 b_toggle.text = getText(R.string.ts_advantages)
-                for (v in ll_container.children) {
-                    val trait = v as TraitView
-                    if (trait.getXp() > 0) {
-                        trait.visibility = View.VISIBLE
-                    } else {
-                        trait.visibility = View.GONE
-                    }
-
-                    val name = trait.getName().toLowerCase()
-                    if (search.length >= 1 && search !in name) {
-                        trait.visibility = View.GONE
-                    }
-                }
-            }
-            2 -> {
+            } else if (toggle == 2 && trait.xp < 0) {
+                filtered_traits += trait
                 b_toggle.text = getText(R.string.ts_disadvantages)
-                for (v in ll_container.children) {
-                    val trait = v as TraitView
-                    if (trait.getXp() < 0) {
-                        trait.visibility = View.VISIBLE
-                    } else {
-                        trait.visibility = View.GONE
-                    }
-
-                    val name = trait.getName().toLowerCase()
-                    if (search.length >= 1 && search !in name) {
-                        trait.visibility = View.GONE
-                    }
-                }
             }
         }
-        */
+
+        rv_adapter.updateData(filtered_traits)
     }
 
+    /**
+     * Load available traits from the SQLite database
+     * @return an Array of [TraitData]
+     */
     fun loadTraits(): Array<TraitData> {
         var traits = arrayOf<TraitData>()
         var sql = """
@@ -203,6 +184,10 @@ class TraitSelectFragment : Fragment() {
         return traits
     }
 
+    /**
+     * [TextWatcher] implementation to read changes in the search field
+     * calls the [toggleTraits] method.
+     */
     class TextChanged: TextWatcher {
         constructor(search: EditText) {
             this.search = search
