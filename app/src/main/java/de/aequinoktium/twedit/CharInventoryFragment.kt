502d70b24f74b365fde3b56dd80254bb863d4415
EricPoehlsen <eric@eric-poehlsen>
2020-09-20 09:21:26 +0200

    Adds nicer interface and Search to CharInventoryFragment

--- app/src/main/java/de/aequinoktium/twedit/CharInventoryFragment.kt
+++ app/src/main/java/de/aequinoktium/twedit/CharInventoryFragment.kt
@@ -1,24 +1,24 @@
 package de.aequinoktium.twedit
 
 import android.os.Bundle
-import android.util.Log
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.LinearLayout
-import android.widget.TextView
+import android.widget.*
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import java.util.*
 
 
 class CharInventoryFragment : Fragment(),
-    SettingsDialog.DialogListener
+        SettingsDialog.DialogListener,
+        TextWatcher
 {
     private val c: CharacterViewModel by activityViewModels()
     private val settings: SettingsViewModel by activityViewModels()
@@ -26,6 +26,7 @@ class CharInventoryFragment : Fragment(),
     private var show_packed = false
     private var show_equipped = false
     private var empty_damage = false
+    private var show_all = false
 
     private lateinit var tv_cash: TextView
     private lateinit var rv_container: RecyclerView
@@ -67,27 +68,40 @@ class CharInventoryFragment : Fragment(),
 
         /* this.findNavController().navigate(R.id.action_cinv_to_cinvequip)*/
 
-        val bt_all = view.findViewById<Button>(R.id.cinv_all)
-        bt_all.setOnClickListener{rv_adapter.showAll()}
+        val iv_all = view.findViewById<ImageView>(R.id.cinv_all)
+        iv_all.setOnClickListener{v -> toggleView(v)}
 
         // button: Add Item
-        val bt_new = view.findViewById<Button>(R.id.cinv_new_item)
-        bt_new.setOnClickListener {
+        val iv_new = view.findViewById<ImageView>(R.id.cinv_new_item)
+        iv_new.setOnClickListener {
             this.findNavController().navigate(R.id.action_cinv_to_cat)
         }
 
         val iv_settings = view.findViewById<ImageView>(R.id.cinv_settings)
         iv_settings.setOnClickListener{settings()}
 
+        val et_search = view.findViewById<EditText>(R.id.cinv_search)
+        et_search.addTextChangedListener(this)
+
     }
 
+    fun toggleView(view: View) {
+        view as ImageView
+        show_all = !show_all
+        if (show_all) {
+            rv_adapter.showAll()
+            view.setImageResource(R.drawable.filter_packed)
+        } else {
+            rv_adapter.showBasedOnSettings()
+            view.setImageResource(R.drawable.filter_all)
+        }
+    }
 
 
     fun showItem(iv: ItemView) {
         c.current_item = iv.item
         this.findNavController().navigate(R.id.action_cinv_to_citem)
     }
-
 
     fun settings() {
         val fm = this.parentFragmentManager
@@ -113,6 +127,18 @@ class CharInventoryFragment : Fragment(),
     }
 
     /**
+     * Implementing TextWatcher to track the search field
+     */
+    override fun afterTextChanged(p0: Editable?) {
+        rv_adapter.search = p0.toString()
+        rv_adapter.showSearchResults()
+    }
+    // unused, necessary for TextWatcher implementation
+    override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+    override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+
+    /**
      * Adapter Class for the Recycler View
      */
     class ItemAdapter(val full_inventory: Array<Item>, val frgm: CharInventoryFragment):
@@ -121,6 +147,7 @@ class CharInventoryFragment : Fragment(),
         View.OnLongClickListener
     {
         var inventory = arrayOf<Item>()
+        var search = ""
 
 
         class ViewHolder(val iv: ItemView) : RecyclerView.ViewHolder(iv)
@@ -215,6 +242,22 @@ class CharInventoryFragment : Fragment(),
             notifyDataSetChanged()
         }
 
+        fun showSearchResults() {
+            if (!search.isEmpty()) {
+                inventory = arrayOf()
+                val lc_search = search.toLowerCase(Locale.getDefault())
+                for (item in full_inventory) {
+                    val lc_name = item.name.toLowerCase(Locale.getDefault())
+                    if (lc_search in lc_name) {
+                        inventory += item
+                    }
+                }
+                notifyDataSetChanged()
+            } else {
+                showBasedOnSettings()
+            }
+        }
+
         fun showBasedOnSettings() {
             if (frgm.show_packed) {
                 showAll()
@@ -234,4 +277,6 @@ class CharInventoryFragment : Fragment(),
 
         }
     }
+
+
 }
